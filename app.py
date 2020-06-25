1dc1de5a7fe6acdba9f3126663d6bd014ae9e72f
Raghav Dalmia <raghav.ec.17@nsit.net.in>
2020-06-25 16:00:06 +0530

    update changes

--- app.py
+++ app.py
@@ -1,70 +1,90 @@
 from flask import Flask, render_template, request
-from gui import init_gui
 from dbmain import main
-
-import tkinter as tk
-from tkinter import filedialog
+from multiprocessing import Process
+import webbrowser
+import sys
+import time
+import os, signal
 
 app = Flask(__name__)
 
 
+def start_server():
+    last_hit = time.time()
+    with open(".tmp", "w") as f:
+        f.write(str(last_hit))
+    
+    app.run()
+
+
+def shutdown_server():
+    server.terminate()
+    os.remove(".tmp")
+    sys.exit(0)
+
+
+def check_time_out():
+    timeout = 10
+    while(True):
+        current_time = time.time()
+
+        with open(".tmp", "r") as f:
+            last_hit = float(f.readline()[:-1])
+
+        if(current_time - last_hit > timeout):
+            print("closing server..")
+            shutdown_server()
+            break
+        else:
+            print("app is running..")
+
+        time.sleep(10)
+
+
+
+# pyinstaller -w -F --onefile --add-data "templates;templates" --add-data "static;static" app.py
 
 @app.route("/")
 def home():
-    status = "Connected to {}".format(db_name) if dao.get_status()=="connected" else "Not connected"
-    return render_template("index.html", Status=status)
+    last_hit = time.time()
 
-@app.route("/connect_to_db", methods=["POST"])
-def render_connect_to_db_page():
+    with open(".tmp", "r+") as f:
+        f.write(str(last_hit))
+
     return render_template("connection.html")
 
+@app.route("/connector/<string:db_name>/<string:db_password>/<string:db_ip>/<string:db_port>/<string:db_sid>")
+def connect_to_db(db_name,db_password,db_ip,db_port,db_sid):
+    last_hit = time.time()
 
-@app.route("/connector", methods=["POST"])
-def connect_to_db():
-    if(request.method=="POST"):
-        
-        db_name = request.form["db_name"]
-        db_password = request.form["db_password"]
-        db_ip = request.form["db_ip"]
-        db_port = request.form["db_port"]
+    with open(".tmp", "r+") as f:
+        f.write(str(last_hit))
 
-        dao.auth(db_name,db_password,db_ip,db_port,'xe')
+    # status = dao.auth(db_name,db_password,db_ip,db_port,db_sid)
+    return {"messgae":status}
 
-    status = "Connected to {}".format(db_name) if dao.get_status()=="Connected" else "Not connected"
-    return render_template("index.html", Status=status)
+@app.route("/awake_hit")
+def close_server():
+    last_hit = time.time()
 
-@app.route("/import_file", methods=["POST"])
-def import_data_into_csv():
-    status = "Connected to {}".format(db_name) if dao.get_status()=="connected" else "Not connected"
-    if(request.method=="POST"):
-        source_file_name = request.form["source_file_text"]
-        destination_file_name = request.form["destination_file_text"]
-        
-        status = dao.service(source_file_name, destination_file_name)
+    with open(".tmp", "r+") as f:
+        f.write(str(last_hit))
 
-    return render_template("index.html", Status=status)
-
-@app.route("/file_path")
-def get_file_path():
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename()
-    root.destroy()
-
-    return file_path if isinstance(file_path, str) else ""
-
-@app.route("/dir_path")
-def get_dir_path():
-    root = tk.Tk()
-    root.withdraw()
-    dir_path = filedialog.askdirectory()
-    root.destroy()
-
-    return (dir_path + ("/" if dir_path[-1]!='/' else ""))if isinstance(dir_path, str) else ""
-
+    print("awake hit, hit time : ",last_hit)
+    return ""
 
 if __name__ == "__main__":
-    global dao, db_name
+    global dao, server, browser, check
+
     dao = main()
-    db_name = dao.get_db_name()
-    init_gui(app)
+
+    server = Process(target=start_server)
+    server.start()
+
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
+    check = Process(target=check_time_out)
+    check.start()
+
+    server.join()
+    check.join()
